6003081b57de0e4dd1fb0346cdb389f0b1dcf8b6
David Jorge-Bates <jorgebd@stpaulsschool.org.uk>
2023-10-12 15:29:35 +0100

    Add project files.

---
+++ C# strings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
